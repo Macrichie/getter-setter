1b1db1d72fcf38efd9b80332b70c3191b9f4444d
Macrichie <makanjuolakunle@gmail.com>
2022-01-27 18:24:07 +0000

    Add .env file

--- utils.js
+++ utils.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const Web3 = require("web3");
 
 const web3 = new Web3("http://localhost:8545");
 
-const privateKey =
-  "0xd3a5bcb2d306cbc5b78836df6c529b96a7a1be269d585b25ed67c01727e3de48";
+const privateKey = process.env.PrivateKey;
+
 const myAccount = web3.eth.accounts.wallet.add(privateKey);
 
 module.exports = {
